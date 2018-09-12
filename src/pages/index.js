33d1e08ede2d36d7ec47c3c7585a02cdfd19f398
Will Worth <will.worth@gmail.com>
2018-09-12 16:42:42 +0200

    add basic img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import will from "../images/bw.jpg" 
 import Layout from '../components/layout'
 
 const IndexPage = () => (
@@ -8,7 +8,7 @@ const IndexPage = () => (
     <h1>You've reached my code site.  Welcome.  Thanks for visting.</h1>
     <p>My name's Will -I'm a Javascript and Python focused coder with some professional experience.
     <em> I'm looking for a full time role as a software developer.</em> </p>
-   
+    <img src={will} alt="Will" class="willpic"  />
     
     <p> If you have any questions about the contents of this site, or want to talk about collaboration
        (I'm open to opensource work, too), please do get in touch via twitter or  email me willworthcoding  at  gmail dot com </p>
